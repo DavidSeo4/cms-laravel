b36981f95c08c953402062cb14f30cf7ef0cff5a
= <=>
2022-05-24 13:00:33 +0200

    ok

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -14,6 +14,6 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Post::class, 50)->create();
+        factory(App\Models\Post::class, 50)->create();
     }
 }
